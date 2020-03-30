e7eecff497527517775c7dd8b798c4b5fe92220c
ThienLV <le.vinh.thien@framgia.com>
2020-03-30 09:08:07 +0900

    electronBuilder add externals for puppeteer trick

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,9 @@
 process.env.VUE_APP_NAME = require('./package.json').app.name
 
 module.exports = {
-  configureWebpack: {
-    externals: {
-      puppeteer: 'require("puppeteer")' // this is magic
-    }
-  },
   pluginOptions: {
     electronBuilder: {
+      externals: ['puppeteer'], // real magic in here
       builderOptions: {
         productName: process.env.VUE_APP_NAME
       }
